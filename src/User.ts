0991401422855601366fd891e38372dc6245e797
chris <crsayen@gmail.com>
2021-05-12 09:07:29 -0400

    users have Timelines, followers, and Users they follow

--- src/User.ts
+++ src/User.ts
@@ -1,6 +1,23 @@
+import Timeline from './Timeline'
+
 export default class User {
   name: string
+  timeline: Timeline
+  following: Set<User>
+  followers: Set<User>
   constructor(name: string) {
     this.name = name
+    this.timeline = new Timeline(this)
+    this.following = new Set()
+    this.followers = new Set()
+  }
+
+  follow(user: User) {
+    user.addFollower(this)
+    this.following.add(user)
+  }
+
+  addFollower(user: User) {
+    this.followers.add(user)
   }
 }
--- test/User.test.ts
+++ test/User.test.ts
@@ -1,9 +1,32 @@
 import User from '../src/User'
+import Timeline from '../src/Timeline'
 
 describe('User', () => {
   const user = new User('any name')
   it('has a name', () => {
     expect(user).toHaveProperty('name')
     expect(typeof user.name).toBe('string')
+  })
+  it('has a timeline', () => {
+    expect(user).toHaveProperty('timeline')
+    expect(user.timeline).toBeInstanceOf(Timeline)
+  })
+  describe('followers', () => {
+    const otherUser = new User('any other name')
+    it('has Users it follows', () => {
+      expect(user).toHaveProperty('following')
+      expect(user.following).toBeInstanceOf(Set)
+    })
+    it('has followers', () => {
+      expect(user).toHaveProperty('followers')
+      expect(user.followers).toBeInstanceOf(Set)
+    })
+    it('can follow other Users', () => {
+      user.follow(otherUser)
+      expect(user.following).toContain(otherUser)
+    })
+    it('keeps track of other Users that followed it', () => {
+      expect(otherUser.followers).toContain(user)
+    })
   })
 })
